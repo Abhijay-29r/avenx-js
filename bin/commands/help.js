7df3d3688592ec6275035769edcd8b19f5aac38e
nathanschmid08 <nathandion.schmid@gmail.com>
2026-08-10 22:10:12 +0200

    docs(cli): update help documentation for --json flag in check command

--- bin/commands/help.js
+++ bin/commands/help.js
@@ -27,6 +27,7 @@ export function printHelp() {
 
 \x1b[1;36mOptions:\x1b[0m
   \x1b[32m--dry-run, -d\x1b[0m             \x1b[90mPreview actions without writing or deleting any files\x1b[0m
+  \x1b[32m--json, -j\x1b[0m                \x1b[90mOutput check/lint validation diagnostics in JSON format\x1b[0m
   \x1b[32m--version, -v\x1b[0m            \x1b[90mOutput the current version\x1b[0m
     `);
 }
