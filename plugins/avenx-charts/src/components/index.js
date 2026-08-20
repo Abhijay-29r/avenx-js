b5b6252a1e51393cc5d75b93e0c1f55067477d76
nathanschmid08 <nathandion.schmid@gmail.com>
2026-08-20 07:47:51 +0200

    feat(charts): implement ChartLine component with curve smoothing and area fill

---
+++ plugins/avenx-charts/src/components/index.js
@@ -0,0 +1,7 @@
+/**
+ * Component Exports for Avenx Charts
+ * @module plugins/avenx-charts/src/components
+ */
+
+export { BaseChart } from './BaseChart.js';
+export { ChartLine } from './ChartLine.js';
